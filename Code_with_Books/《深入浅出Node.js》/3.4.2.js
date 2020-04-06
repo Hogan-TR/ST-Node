d149e45ea54a2726eed28953815b82825eea93b9
15262856393tr@gmail.com <15262856393tr@gmail.com>
2020-04-07 00:26:00 +0800

    cancel code comments

--- Code_with_Books/《深入浅出Node.js》/3.4.2.js
+++ Code_with_Books/《深入浅出Node.js》/3.4.2.js
@@ -1,47 +1,47 @@
-// // 立即异步执行一个任务
-// setTimeout(function () {
-//     // TODO
-// }, 0); // 浪费性能
+// 立即异步执行一个任务
+setTimeout(function () {
+    // TODO
+}, 0); // 浪费性能
 
-// // 轻量版 更高效
-// // 仅将回调函数放入队列中
-// // process.nextTick = function (callback) {
-// //     // on the way out, don't bother.
-// //     // it won't get fired anyway
-// //     if (process._exiting) return;
+// 轻量版 更高效
+// 仅将回调函数放入队列中
+// process.nextTick = function (callback) {
+//     // on the way out, don't bother.
+//     // it won't get fired anyway
+//     if (process._exiting) return;
 
-// //     if (tickDepth >= process.maxTickDepth)
-// //         maxTickWarn();
+//     if (tickDepth >= process.maxTickDepth)
+//         maxTickWarn();
 
-// //     var tock = { callback: callback };
-// //     if (process.domain) tock.domain = process.domain;
-// //     nextTickQueue.push(tock);
-// //     if (nextTickQueue.length) {
-// //         process._needTickCallback();
-// //     }
-// // };
-// process.nextTick(function(){
-//     console.log('延迟执行');
-// });
-// console.log('正常执行');
+//     var tock = { callback: callback };
+//     if (process.domain) tock.domain = process.domain;
+//     nextTickQueue.push(tock);
+//     if (nextTickQueue.length) {
+//         process._needTickCallback();
+//     }
+// };
+process.nextTick(function(){
+    console.log('延迟执行');
+});
+console.log('正常执行');
 
-// // setImmediate
-// setImmediate(function(){
-//     console.log('延迟执行');
-// });
-// console.log('正常执行');
+// setImmediate
+setImmediate(function(){
+    console.log('延迟执行');
+});
+console.log('正常执行');
 
-// // setImmediate & process.nextTick 优先级比较
-// process.nextTick(() => {
-//     console.log('nextTick延迟执行');
-// });
-// setImmediate(() => {
-//     console.log('setImmediate延迟执行');
-// });
-// console.log('正常执行');
-// // nextTick优先级更高
-// // process.nextTick -> idle观察者
-// // setImmediate    -> check观察者
+// setImmediate & process.nextTick 优先级比较
+process.nextTick(() => {
+    console.log('nextTick延迟执行');
+});
+setImmediate(() => {
+    console.log('setImmediate延迟执行');
+});
+console.log('正常执行');
+// nextTick优先级更高
+// process.nextTick -> idle观察者
+// setImmediate    -> check观察者
 
 // nextTick 保存于数组 全部执行
 // setImmediate 保存于链表 执行一个
